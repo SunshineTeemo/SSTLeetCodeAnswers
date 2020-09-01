9aba124a977c9095e0232eb8e43598488cf3cf7a
SST <1367813722@qq.com>
2020-09-01 18:31:09 +0800

    196

--- SSTLeetCodeAnswers/main.swift
+++ SSTLeetCodeAnswers/main.swift
@@ -9,3 +9,69 @@
 import Foundation
 
 
+
+
+//MARK:- 198.打家劫舍
+//https://leetcode-cn.com/problems/house-robber/
+func rob(_ nums: [Int]) -> Int {
+    
+    
+    if nums.isEmpty {
+        return 0
+    }
+    
+    var max = 0
+    
+    var secondMax = 0
+    var maxIndex = 0
+    
+    
+    for (index,value) in nums.enumerated() {
+        
+        if index == 0 {
+            max = value
+
+        }else{
+            
+            if index - maxIndex == 1 {
+                
+                if  max < value + secondMax{
+                    
+                    let temp = max
+                    max = value + secondMax
+
+                    secondMax = temp
+                    maxIndex = index
+                    
+                    
+                }else{
+                    secondMax = value + secondMax
+                    
+                    
+                }
+               
+                
+            }else if index - maxIndex > 1{
+              
+                secondMax = max
+                max = max + value
+                maxIndex = index
+                
+                
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    return max
+}
+
+
+
+print(rob([2,1,1,2]))
+
+
+
