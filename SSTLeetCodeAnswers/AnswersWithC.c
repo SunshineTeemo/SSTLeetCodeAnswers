5f588221222a3115a6d05938925a896367e62599
SST <1367813722@qq.com>
2020-09-01 16:52:47 +0800

    清空多余代码

--- SSTLeetCodeAnswers/AnswersWithC.c
+++ SSTLeetCodeAnswers/AnswersWithC.c
@@ -1,11 +1,14 @@
 //
-//  main.swift
+//  AnswersWithC.c
 //  SSTLeetCodeAnswers
 //
 //  Created by 梅琰培 on 9/1/20.
 //  Copyright © 2020 SunshineTeemo. All rights reserved.
 //
 
-import Foundation
+#include "AnswersWithC.h"
+
+
+
 
 
--- SSTLeetCodeAnswers/AnswersWithC.h
+++ SSTLeetCodeAnswers/AnswersWithC.h
@@ -1,11 +1,17 @@
 //
-//  main.swift
+//  AnswersWithC.h
 //  SSTLeetCodeAnswers
 //
 //  Created by 梅琰培 on 9/1/20.
 //  Copyright © 2020 SunshineTeemo. All rights reserved.
 //
 
-import Foundation
+#ifndef AnswersWithC_h
+#define AnswersWithC_h
+
+#include <stdio.h>
 
 
+
+
+#endif /* AnswersWithC_h */
