d6d245b913a2053e4be0965bd4f684ef71ee7426
梅琰培 <Mei_YanPei@want-want.com>
2020-09-01 16:25:04 +0800

    Initial Commit

---
+++ SSTLeetCodeAnswers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SSTLeetCodeAnswers
+//
+//  Created by 梅琰培 on 9/1/20.
+//  Copyright © 2020 SunshineTeemo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
